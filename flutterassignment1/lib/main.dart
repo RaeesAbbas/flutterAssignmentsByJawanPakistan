1c1d320f35d203f237421c979449579ea5eee3f4
Raees Abbas <raeesabbas783@gmail.com>
2021-05-15 21:57:08 +0500

    Assignment 1 completed app is in running state

---
+++ flutterassignment1/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World Demo Application',
+      home: (Scaffold(
+        body: Text(
+            'Hello world , Welcome to Flutter App Development Course By Jawan Pakistan '),
+      )),
+    );
+  }
+}
